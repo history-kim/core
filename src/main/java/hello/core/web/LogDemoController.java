a4054f520201c48ef4ddf30b10a96ae2697cce37
history <history-kim@naver.com>
2021-04-17 18:11:26 +0900

    주석추가

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller 
 @RequiredArgsConstructor //선언된 모든 final 필드가 포함된 생성자를 만든다
-public class LogDemoController {
+public class LogDemoController { //
 
     private final LogDemoService logDemoService;
     //private final ObjectProvider<MyLogger> myLoggerProvider;
